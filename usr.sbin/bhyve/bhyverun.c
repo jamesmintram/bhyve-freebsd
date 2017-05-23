8868cbc629ed89ed4963493da376523a7674e149
Flavius Anton <f.v.anton@gmail.com>
2017-05-23 17:20:58 +0300

    Save/Restore virtio-blk device state.

--- usr.sbin/bhyve/bhyverun.c
+++ usr.sbin/bhyve/bhyverun.c
@@ -1484,6 +1484,7 @@ restore_pci_devs(struct vmctx *ctx, struct restore_state *rstate)
 	int i;
 	char *devs[] = {
 		"virtio-net",
+		"virtio-blk",
 	};
 
 	for (i = 0; i < sizeof(devs)/sizeof(devs[0]); i++) {
@@ -1597,6 +1598,7 @@ vm_snapshot_pci_data(struct vmctx *ctx, int data_fd, xo_handle_t *xop)
 	void *buffer = NULL;
 	char *devs[] = {
 		"virtio-net",
+		"virtio-blk",
 	};
 
 	offset = lseek(data_fd, 0, SEEK_CUR);
