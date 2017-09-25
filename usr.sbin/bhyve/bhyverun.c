034fbf216a3c2cedc3a7de0fbb9e9fdefdf3ebd8
Elena Mihailescu <elenamihailescu22@gmail.com>
2017-09-25 14:09:35 +0300

    Add snapshot and restore functions for vatpit structure
    Signed-off-by: Elena Mihailescu <elenamihailescu22@gmail.com>

--- usr.sbin/bhyve/bhyverun.c
+++ usr.sbin/bhyve/bhyverun.c
@@ -1459,6 +1459,7 @@ restore_kernel_structs(struct vmctx *ctx, struct restore_state *rstate)
 		STRUCT_VHPET,
 		STRUCT_VMCX,
 		STRUCT_VATPIC,
+		STRUCT_VATPIT,
 	};
 
 	for (i = 0; i < sizeof(structs)/sizeof(structs[0]); i++) {
@@ -1523,9 +1524,10 @@ vm_snapshot_kern_data(struct vmctx *ctx, int data_fd, xo_handle_t *xop)
 		STRUCT_VHPET,
 		STRUCT_VMCX,
 		STRUCT_VATPIC,
+		STRUCT_VATPIT,
 	};
 
-	char *snapshot_struct_names[] = {"vm", "vmx", "vioapic", "vlapic", "lapic", "vhpet", "vmcs", "vatpic"};
+	char *snapshot_struct_names[] = {"vm", "vmx", "vioapic", "vlapic", "lapic", "vhpet", "vmcs", "vatpic", "vatpit"};
 
 	buffer = malloc(SNAPSHOT_BUFFER_SIZE * sizeof(char));
 	if (buffer == NULL) {
