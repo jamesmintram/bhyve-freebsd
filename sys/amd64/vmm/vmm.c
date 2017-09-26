79536eb8886b52afdb426fbd21b7814e9b20ffc6
Elena Mihailescu <elenamihailescu22@gmail.com>
2017-09-26 17:37:38 +0300

    Add snapshot & restore functions for vrtc structure
    Signed-off-by: Elena Mihailescu <elenamihailescu22@gmail.com>

--- sys/amd64/vmm/vmm.c
+++ sys/amd64/vmm/vmm.c
@@ -2880,6 +2880,12 @@ vm_snapshot_vpmtmr(struct vm *vm, void *buffer, size_t buf_size, size_t *snapsho
 }
 
 static int
+vm_snapshot_vrtc(struct vm *vm, void *buffer, size_t buf_size, size_t *snapshot_size)
+{
+	return vrtc_snapshot(vm_rtc(vm), buffer, buf_size, snapshot_size);
+}
+
+static int
 vm_snapshot_vmcx(struct vm *vm, void *buffer, size_t buf_size,
 		 size_t *snapshot_size)
 {
@@ -2952,6 +2958,9 @@ vm_snapshot_req(struct vm *vm, enum snapshot_req req, void *buffer,
 		break;
 	case STRUCT_VPMTMR:
 		ret = vm_snapshot_vpmtmr(vm, buffer, buf_size, snapshot_size);
+		break;
+	case STRUCT_VRTC:
+		ret = vm_snapshot_vrtc(vm, buffer, buf_size, snapshot_size);
 		break;
 	default:
 		printf("%s: failed to find the requested type\n", __func__);
@@ -3055,6 +3064,12 @@ vm_restore_vpmtmr(struct vm *vm, void *buffer, size_t buf_size)
 }
 
 static int
+vm_restore_vrtc(struct vm *vm, void *buffer, size_t buf_size)
+{
+	return vrtc_restore(vm_rtc(vm), buffer, buf_size);
+}
+
+static int
 vm_restore_vmcx(struct vm *vm, void *buffer, size_t buf_size)
 {
 	int i, error;
@@ -3124,6 +3139,9 @@ vm_restore_req(struct vm *vm, enum snapshot_req req, void *buffer, size_t buf_si
 		break;
 	case STRUCT_VPMTMR:
 		ret = vm_restore_vpmtmr(vm, kbuf, buf_size);
+		break;
+	case STRUCT_VRTC:
+		ret = vm_restore_vrtc(vm, kbuf, buf_size);
 		break;
 	default:
 		printf("%s: failed to find type to restore\n", __func__);
