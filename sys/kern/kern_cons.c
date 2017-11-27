ffeb3d7962794d33e91fd9c45ef5357a7fde21ff
Alexandru Elisei <alexandru.elisei@gmail.com>
2017-11-27 12:22:35 +0200

    kern/kern_cons.c: Remove debugging

--- sys/kern/kern_cons.c
+++ sys/kern/kern_cons.c
@@ -107,8 +107,6 @@ static struct consdev cons_consdev;
 DATA_SET(cons_set, cons_consdev);
 SET_DECLARE(cons_set, struct consdev);
 
-char *cn_name;
-
 void
 cninit(void)
 {
@@ -118,7 +116,6 @@ cninit(void)
 	 * Check if we should mute the console (for security reasons perhaps)
 	 * It can be changes dynamically using sysctl kern.consmute
 	 * once we are up and going.
-	 * 
 	 */
         cn_mute = ((boothowto & (RB_MUTE
 			|RB_SINGLE
@@ -147,8 +144,8 @@ cninit(void)
 			cn->cn_ops->cn_init(cn);
 			cnadd(cn);
 		}
-		cn_name = cn->cn_name;
 	}
+
 	if (best_cn == NULL)
 		return;
 	if ((boothowto & RB_MULTIPLE) == 0) {
