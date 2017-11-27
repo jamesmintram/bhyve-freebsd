fadd5ca9af0ec475eff89e2afd0cc436ccbb7bec
Alexandru Elisei <alexandru.elisei@gmail.com>
2017-11-27 11:46:57 +0200

    Remove guest specific machdep file machdep_guest.c

--- sys/arm64/arm64/machdep.c
+++ sys/arm64/arm64/machdep.c
@@ -958,15 +958,11 @@ print_efi_map_entries(struct efi_map_header *efihdr)
 	foreach_efi_map_entry(efihdr, print_efi_map_entry);
 }
 
-// TODO: delete me
-vm_offset_t dtbp;
-
 #ifdef FDT
 static void
 try_load_dtb(caddr_t kmdp)
 {
-	// TODO: uncomment me
-	//vm_offset_t dtbp;
+	vm_offset_t dtbp;
 
 	dtbp = MD_FETCH(kmdp, MODINFOMD_DTBP, vm_offset_t);
 #if defined(FDT_DTB_STATIC)
@@ -1083,10 +1079,6 @@ cache_setup(void)
 	}
 }
 
-// TODO: delete me
-vm_offset_t lastaddr;
-vm_offset_t envp;
-
 void
 initarm(struct arm64_bootparams *abp)
 {
@@ -1098,8 +1090,7 @@ initarm(struct arm64_bootparams *abp)
 	struct mem_region mem_regions[FDT_MEM_REGIONS];
 	int mem_regions_sz;
 #endif
-	// TODO: Uncomment me
-	//vm_offset_t lastaddr;
+	vm_offset_t lastaddr;
 	caddr_t kmdp;
 	bool valid;
 
@@ -1112,7 +1103,6 @@ initarm(struct arm64_bootparams *abp)
 		kmdp = preload_search_by_type("elf64 kernel");
 
 	link_elf_ireloc(kmdp);
-	envp = MD_FETCH(kmdp, MODINFOMD_ENVP, vm_offset_t);
 	try_load_dtb(kmdp);
 
 	efi_systbl_phys = MD_FETCH(kmdp, MODINFOMD_FW_HANDLE, vm_paddr_t);
