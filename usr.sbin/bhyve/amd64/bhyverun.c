582c047c4d43d1d4db0e24812f94c1d3ea186596
Darius Mihai <dariusmihaim@gmail.com>
2018-04-27 14:15:32 +0300

    Continue emulated device integration - amd64/PCI
    Signed-off-by: Darius Mihai <dariusmihaim@gmail.com>

--- usr.sbin/bhyve/amd64/bhyverun.c
+++ usr.sbin/bhyve/amd64/bhyverun.c
@@ -73,7 +73,7 @@ __FBSDID("$FreeBSD$");
 #include "mevent.h"
 #include "mptbl.h"
 #include "devemu.h"
-#include "pci_irq.h"
+#include "devemu_irq.h"
 #include "pci_lpc.h"
 #include "smbiostbl.h"
 #include "xmsr.h"
@@ -838,7 +838,7 @@ main(int argc, char *argv[])
 			}
 			break;
 		case 's':
-			if (pci_parse_slot(optarg) != 0)
+			if (devemu_parse_slot(optarg) != 0)
 				exit(1);
 			else
 				break;
@@ -931,7 +931,7 @@ main(int argc, char *argv[])
 	init_mem();
 	init_inout();
 	atkbdc_init(ctx);
-	pci_irq_init(ctx);
+	devemu_irq_init(ctx);
 	ioapic_init(ctx);
 
 	rtc_init(ctx, rtc_localtime);
@@ -940,7 +940,7 @@ main(int argc, char *argv[])
 	/*
 	 * Exit if a device emulation finds an error in its initilization
 	 */
-	if (init_pci(ctx) != 0)
+	if (init_devemu(ctx) != 0)
 		exit(1);
 
 	if (gdb_port != 0)
