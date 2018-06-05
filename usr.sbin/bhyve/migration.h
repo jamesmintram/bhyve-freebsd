e38f754da24cfc01772d291f31440f8770456e5f
Elena Mihailescu <elenamihailescu22@gmail.com>
2018-06-05 11:58:06 +0300

    Move warm migration implementation from libvmmapi to usr.sbin/bhyve/migration.c
    Signed-off-by: Elena Mihailescu <elenamihailescu22@gmail.com>

--- usr.sbin/bhyve/migration.h
+++ usr.sbin/bhyve/migration.h
@@ -5,4 +5,29 @@ struct vmctx;
 
 int receive_vm_migration(struct vmctx *ctx, char *migration_data);
 
+/* Warm Migration */
+
+enum message_types {
+    MESSAGE_TYPE_SPECS	    = 1,
+    MESSAGE_TYPE_METADATA   = 2,
+    MESSAGE_TYPE_RAM	    = 3,
+    MESSAGE_TYPE_KERN	    = 4,
+    MESSAGE_TYPE_PCI	    = 5,
+};
+
+struct __attribute__((packed)) migration_message_type {
+    size_t len;
+    unsigned int type;		// enum message_type
+    unsigned int req_type;	// enum snapshot_req
+};
+
+struct __attribute__((packed)) migration_system_specs {
+	char hw_machine[MAX_SPEC_LEN];
+	char hw_model[MAX_SPEC_LEN];
+	size_t hw_pagesize;
+};
+
+int vm_send_migrate_req(struct vmctx *ctx, struct migrate_req req, void *pci_ptr);
+int vm_recv_migrate_req(struct vmctx *ctx, struct migrate_req req, void *pci_ptr);
+
 #endif
