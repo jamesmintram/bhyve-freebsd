248c75bbe76eb9028d6f4cf41dd98242d2c31444
Elena Mihailescu <elenamihailescu22@gmail.com>
2019-09-05 15:36:33 +0300

    Add setter and getter functions for pages
    These functions are used by the live migration feature for bhyve
    
    Signed-off-by: Elena Mihailescu <elenamihailescu22@gmail.com>

--- sys/vm/vm_object.c
+++ sys/vm/vm_object.c
@@ -2362,6 +2362,54 @@ SYSCTL_PROC(_vm, OID_AUTO, objects, CTLTYPE_STRUCT | CTLFLAG_RW | CTLFLAG_SKIP |
     CTLFLAG_MPSAFE, NULL, 0, sysctl_vm_object_list, "S,kinfo_vmobject",
     "List of VM objects");
 
+int
+vm_object_get_page(vm_object_t object, vm_pindex_t pindex, void *dst)
+{
+	vm_page_t page;
+	vm_offset_t page_src;
+
+	VM_OBJECT_ASSERT_WLOCKED(object);
+
+	page = vm_page_lookup(object, pindex);
+	if (page == NULL) {
+		// cannot find page
+		return (-1);
+	}
+
+	vm_page_xbusy(page);
+	page->oflags &= ~VPO_VMM_DIRTY;
+	vm_page_xunbusy(page);
+
+	pmap_clear_modify(page);
+
+	vm_page_xbusy(page);
+	page_src = PHYS_TO_DMAP(VM_PAGE_TO_PHYS(page));
+	memcpy(dst, (void *)page_src, PAGE_SIZE);
+	vm_page_xunbusy(page);
+
+	return (0);
+}
+
+int
+vm_object_set_page(vm_object_t object, vm_pindex_t pindex, void *src)
+{
+	vm_page_t page;
+	vm_offset_t page_src;
+
+	VM_OBJECT_ASSERT_WLOCKED(object);
+
+	page = vm_page_lookup(object, pindex);
+	if (page == NULL) {
+		// cannot find page
+		return (-1);
+	}
+
+	page_src = PHYS_TO_DMAP(VM_PAGE_TO_PHYS(page));
+	memcpy((void *)page_src, src, PAGE_SIZE);
+
+	return (0);
+}
+
 #include "opt_ddb.h"
 #ifdef DDB
 #include <sys/kernel.h>
