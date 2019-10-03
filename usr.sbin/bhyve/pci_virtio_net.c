1c8079c81aac04128898dc0fc024bcd4e8c6fd2d
Darius Mihai <dariusmihaim@gmail.com>
2019-10-03 11:59:29 +0300

    Force reapply features when restoring vtnet
    Without applying the features, various errors were encountered after a
    restore which caused packet drops.
    
    Special thanks to Aleksandr Fedorov and Kirill Ponomarev.
    
    Signed-off-by: Darius Mihai <dariusmihaim@gmail.com>

--- usr.sbin/bhyve/pci_virtio_net.c
+++ usr.sbin/bhyve/pci_virtio_net.c
@@ -279,6 +279,11 @@ pci_vtnet_snapshot(void *vsc, struct vm_snapshot_meta *meta)
 	 */
 
 	SNAPSHOT_VAR_OR_LEAVE(sc->vsc_features, meta, ret, done);
+
+	/* Force reapply negociated features at restore time */
+	if (meta->op == VM_SNAPSHOT_RESTORE)
+		pci_vtnet_neg_features(sc, sc->vsc_features);
+
 	SNAPSHOT_VAR_OR_LEAVE(sc->vsc_config, meta, ret, done);
 	SNAPSHOT_VAR_OR_LEAVE(sc->rx_vhdrlen, meta, ret, done);
 	SNAPSHOT_VAR_OR_LEAVE(sc->rx_merge, meta, ret, done);
