5225a4cacc052600ad4d12db17dcf31dd2544ea5
Eric Postolache <epostolache@gmail.com>
2020-03-29 09:38:55 +0300

    First commit of multiple devices save/restore

--- usr.sbin/bhyve/snapshot.h
+++ usr.sbin/bhyve/snapshot.h
@@ -26,9 +26,9 @@ struct checkpoint_thread_info {
 	struct sockaddr_un *addr;
 } checkpoint_info;
 
-typedef int (*vm_snapshot_dev_cb)(struct vm_snapshot_meta *);
-typedef int (*vm_pause_dev_cb) (struct vmctx *, const char *);
-typedef int (*vm_resume_dev_cb) (struct vmctx *, const char *);
+typedef int (*vm_snapshot_dev_cb)(struct vm_snapshot_meta *, void *dev_meta);
+typedef int (*vm_pause_dev_cb) (struct vmctx *, const char *, void *dev_meta);
+typedef int (*vm_resume_dev_cb) (struct vmctx *, const char *, void *dev_meta);
 
 struct vm_snapshot_dev_info {
 	const char *dev_name;		/* device name */
@@ -42,6 +42,18 @@ struct vm_snapshot_kern_info {
 	enum snapshot_req req;		/* request type */
 };
 
+struct vm_snapshot_registered_devs {
+	//int bus, slot, func;
+	char *dev_name;
+	// for each device type, the meta should be specific
+	void *meta_data;
+	size_t meta_size;
+	struct vm_snapshot_registered_devs *next_dev;
+};
+
+struct vm_snapshot_registered_devs *head_registered_devs;
+void pushback_registered_devs(char *dev_name, void *meta_data, size_t meta_size);
+struct vm_snapshot_registered_devs *copy_registered_devs();
 
 void destroy_restore_state(struct restore_state *rstate);
 
@@ -56,8 +68,10 @@ void checkpoint_cpu_suspend(int vcpu);
 
 int restore_vm_mem(struct vmctx *ctx, struct restore_state *rstate);
 int vm_restore_kern_structs(struct vmctx *ctx, struct restore_state *rstate);
-
-int vm_restore_user_devs(struct vmctx *ctx, struct restore_state *rstate);
+int vm_restore_user_dev(struct vmctx *ctx, struct restore_state *rstate, void *dev_ptr,
+	size_t dev_size, char *dev_name, void *dev_meta);
+int vm_restore_user_devs(struct vmctx *ctx, struct restore_state *rstate, 
+				struct vm_snapshot_registered_devs **head_registered_devs);
 int vm_pause_user_devs(struct vmctx *ctx);
 int vm_resume_user_devs(struct vmctx *ctx);
 
